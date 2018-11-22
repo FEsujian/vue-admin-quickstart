e5836eb4bc0472b6343207bd5d28b509ddede980
maowulin <18829224680@163.com>
2018-11-22 15:24:30 +0800

    修改LOGIN

--- src/core/bootstrap/plugin.init.ts
+++ src/core/bootstrap/plugin.init.ts
@@ -2,7 +2,10 @@ import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import 'normalize.css';
+import axios from 'axios';
 
 export default async function() {
   Vue.use(ElementUI);
+  Vue.prototype.$axios = axios;
+  axios.defaults.baseURL = 'http://localhost:8086';
 }
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,6 +1,14 @@
 <template>
-  <section class="page login">
-    <el-button type="primary">登陆</el-button>
+  <section class="page login bg">
+    <header></header>
+    <main>
+      <div class="ft24 text-center mt10">登录</div>
+      <img src="../assets/images/bg.jpg" alt="">
+      <el-input type="primary" placeholder="请输入账号" v-model="admin"></el-input>
+      <el-input type="primary" placeholder="请输入密码" v-model="password"></el-input>
+      <el-button type="primary" @click="login" style="width:100%">登录</el-button>
+    </main>
+    <div class="footer" style="">123456</div>
   </section>
 </template>
 
@@ -13,5 +21,50 @@ import HelloWorld from '@/components/HelloWorld.vue';
     HelloWorld,
   },
 })
-export default class Login extends Vue {}
+export default class Login extends Vue {
+  private admin: string = '';
+  private password: string = '';
+  private login() {
+    if (!this.admin) {
+      this.$message.error('请输入账号');
+    } else if (!this.password) {
+      this.$message.error('请输入密码');
+    } else {
+      this.$router.push({ path: '/admin' });
+    }
+  }
+}
 </script>
+<style lang="less" scoped>
+.bg {
+  background: url('../assets/images/bg.jpg') center no-repeat;
+  background-size: 100%;
+}
+header,
+.footer {
+  height: 80px;
+  border: 1px solid grey;
+  width: 100%;
+  line-height: 80px;
+  text-align: center;
+}
+main {
+  width: 250px;
+  height: 250px;
+  // border: 1px solid grey;
+  padding: 50px;
+  border-radius: 5px;
+  position: fixed;
+  top: 220px;
+  right: 400px;
+  box-shadow: 5px 5px 5px #949292;
+  background-color: rgba(000, 000, 000, 0.1);
+}
+main div {
+  height: 60px;
+}
+.footer {
+  position: fixed;
+  bottom: 0;
+}
+</style>
